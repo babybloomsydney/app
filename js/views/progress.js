99f7d396230e0d040c2650c749b737c56b794a36
babybloomsydney <bailey@babybloomsydney.com.au>
2025-12-12 12:42:42 +1100

    Update progress.js

--- js/views/progress.js
+++ js/views/progress.js
@@ -5,7 +5,22 @@ const ProgressView = {
             const d = Utils.prepChart(res.data.stats);
             const ctx = document.getElementById('radarChart');
             if(window.myRadar) window.myRadar.destroy();
-            window.myRadar = new Chart(ctx,{type:'radar',data:{labels:d.labels,datasets:[{label:'Mastery',data:d.values,backgroundColor:'rgba(16,185,129,0.2)',borderColor:'#10b981'}]},options:{scales:{r:{beginAtZero:true,max:100,ticks:{display:false}}},plugins:{legend:{display:false}}}});
+            window.myRadar = new Chart(ctx,{
+                type:'radar',
+                data:{
+                    labels: d.labels,
+                    datasets:[{
+                        label: TXT.VIEWS.PROGRESS_DASHBOARD.HEADER_CHART,
+                        data: d.values,
+                        backgroundColor:'rgba(16,185,129,0.2)',
+                        borderColor:'#10b981'
+                    }]
+                },
+                options:{
+                    scales:{r:{beginAtZero:true,max:100,ticks:{display:false}}},
+                    plugins:{legend:{display:false}}
+                }
+            });
         }
     }
 };
