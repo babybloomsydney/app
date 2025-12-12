023ea4274d2d2ce31383f2faf998b824e3e7cdaa
babybloomsydney <bailey@babybloomsydney.com.au>
2025-12-12 12:37:16 +1100

    Update general.js

--- js/components/modals/observation/general.js
+++ js/components/modals/observation/general.js
@@ -1,21 +1,19 @@
 /**
  * OBSERVATION: GENERAL
+ * GENERAL
  */
 const ObsGeneral = {
     submit: async () => {
         const note = document.getElementById('logNoteGeneral').value;
-        if(!note.trim()) return alert("Please write a note.");
-
+        if(!note.trim()) return alert("Please write a observation.");
         const btn = document.getElementById('btnSubmitGeneral');
         const oldText = btn.innerText;
-        btn.innerHTML = `<i class="fa-solid fa-check mr-2"></i> Added!`;
+        btn.innerHTML = `<i class="fa-solid fa-check mr-2"></i> ${TXT.COMPONENTS.MODALS.OBSERVATION.SUCCESS_MSG}`;
         btn.classList.add('btn-success');
         btn.disabled = true;
-
         await API.logObservation(STATE.child.childId, "General", null, null, note);
-
         setTimeout(() => {
-            btn.innerText = oldText; 
+            btn.innerText = oldText;
             btn.classList.remove('btn-success');
             btn.disabled = false;
             Modals.close();
