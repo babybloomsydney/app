08b79163c19e8dc6b73256deb6f451964d0429a4
babybloomsydney <bailey@babybloomsydney.com.au>
2025-12-17 21:00:28 +1100

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    // *** 2.2.2 ***
+    // *** 2.2.1 ***
     API_URL: "https://script.google.com/macros/s/AKfycbz-TBEBEurXzGlEbIHM1cbb23bTZJ1QyF9lNNt9mB6xISNSyckmoS_zUoZZsXJYQKbV/exec",
     DOMAINS: { "CL":"Communication", "PSE":"Social", "PD":"Physical", "LIT":"Literacy", "NUM":"Numeracy", "UW":"World", "EAD":"Art" }
 };
