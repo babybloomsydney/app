934f7ab7e143a69de127ca1102fab21d6cc87d7c
babybloomsydney <bailey@babybloomsydney.com.au>
2026-02-02 16:58:18 +1100

    Update API_URL to new endpoint for version 2.5.1.5

--- js/core/config.js
+++ js/core/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    // *** 2.4.1 ***
-    API_URL: "https://script.google.com/macros/s/AKfycbz0qAgvGeBMOyfNfKe7elc23GvE-D5bs4b7wHjBVhe4BU7_nwpLtFrVyJd335_aIfLE/exec",
+    // *** 2.5.1.5 ***
+    API_URL: "https://script.google.com/macros/s/AKfycbziPv3KDlu19qJZCV-tvBoE4mV7DcdIp2CfVmH-G63nVPphvpIXQVD6hmzqKP0roa4a/exec",
     DOMAINS: { "CL":"Communication", "PSE":"Social", "PD":"Physical", "LIT":"Literacy", "NUM":"Numeracy", "UW":"World", "EAD":"Art" }
 };
 const STATE = { user: { id: localStorage.getItem('bb_uid'), role: localStorage.getItem('bb_role') }, child: null, library: [], feed: [], selectedObjectives: [], reviewActivityId: null, logRating: null };
