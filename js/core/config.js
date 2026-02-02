f3bd2aad7ae90dc2015174b01fd686428752c6ea
babybloomsydney <bailey@babybloomsydney.com.au>
2026-02-02 16:23:42 +1100

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    // *** 2.5.1.4 ***
-    API_URL: "https://script.google.com/macros/s/AKfycbyW8IbwO8ZWEYmAyojBLGzrbjjNcirQJIjJIgnAO2uH6lAoRKjfX0w4Mk6TbQnoPx6T/exec",
+    // *** 2.5.1.5 ***
+    API_URL: "https://script.google.com/macros/s/AKfycbziPv3KDlu19qJZCV-tvBoE4mV7DcdIp2CfVmH-G63nVPphvpIXQVD6hmzqKP0roa4a/exec",
     DOMAINS: { "CL":"Communication", "PSE":"Social", "PD":"Physical", "LIT":"Literacy", "NUM":"Numeracy", "UW":"World", "EAD":"Art" }
 };
 const STATE = { user: { id: localStorage.getItem('bb_uid'), role: localStorage.getItem('bb_role') }, child: null, library: [], feed: [], selectedObjectives: [], reviewActivityId: null, logRating: null };
